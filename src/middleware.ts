c55b692a635ae966d185bb948c120bd7888be1c7
dough <viktor.melin@me.com>
2023-08-23 10:35:58 +0200

    refactor: auth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-  publicRoutes: ['/'],
+  publicRoutes: ['/', '/search', '/movie(.*)', '/tv(.*)', '/person(.*)'],
 });
 
 export const config = {
