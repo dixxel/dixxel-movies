12b03a430249fb0df15dcec03e25780ede2c5a9b
dough <viktor.melin@me.com>
2023-08-23 10:37:15 +0200

    feat: landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,11 @@ import StateProvider from '@/components/StateProvider';
 import './globals.css';
 import { Poppins } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs';
+import { tmdbFetch } from '@/lib/tmdb';
+import Link from 'next/link';
+import { BaseResponse, BaseResults } from '@/types/tmdb';
+import Navbar from '@/components/Navbar';
+
 const poppins = Poppins({ subsets: ['latin'], weight: ['400', '500', '600', '700', '800'] });
 
 export const metadata = {
@@ -25,13 +30,19 @@ export const metadata = {
 };
 
 const RootLayout = async ({ children }: { children: React.ReactNode }) => {
+  const trending = await tmdbFetch<BaseResponse>('/trending/all/day?language=en-US');
+
   return (
     <ClerkProvider>
       <html lang='en'>
-        <body
-          className={`${poppins.className} bg-gradient-to-bl from-gray-900 to-gray-800 text-white min-h-screen m-0 flex flex-col`}
-        >
-          <main className='flex-1 w-full lg:flex lg:items-center lg:flex-col'>{children}</main>
+        <body className={`${poppins.className}  text-white min-h-screen m-0 flex flex-col`}>
+          <main
+            className='relative flex-1 w-full flex items-center flex-col bg-cover bg-no-repeat bg-left after:absolute after:w-full after:h-full after:bg-gradient-to-r after:from-gray-900/90 after:to-gray-800'
+            style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original/${trending.results[0].poster_path})` }}
+          >
+            <Navbar />
+            {children}
+          </main>
         </body>
       </html>
     </ClerkProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,51 @@
-const Page = () => {
+import SearchBar from '@/components/SearchBar';
+import { tmdbFetch } from '@/lib/tmdb';
+import { BaseResponse } from '@/types/tmdb';
+import Link from 'next/link';
+
+const Page = async () => {
+  const allTrending = await tmdbFetch<BaseResponse>('/trending/all/day?language=en-US');
+  const movieTrending = await tmdbFetch<BaseResponse>('/trending/movie/day?language=en-US');
+  const tvTrending = await tmdbFetch<BaseResponse>('/trending/tv/day?language=en-US');
+
   return (
-    <>
-      <div className='p-5 flex justify-center items-center flex-col lg:px-0 lg:max-w-[1000px] lg:w-full'>
-        <p>Hello World</p>
+    <div className='relative flex-1 z-10 p-5 flex justify-center items-center flex-col gap-5 px-0 max-w-[1000px] w-full'>
+      <Link
+        href='/all'
+        className='relative flex-1 w-full flex items-center justify-center bg-cover bg-center bg-no-repeat rounded-md shadow-lg cursor-pointer group after:absolute after:w-full after:h-full after:bg-gradient-to-r after:from-blue-500/10 after:to-blue-900/70 after:rounded-md'
+        style={{
+          backgroundImage: `url(https://image.tmdb.org/t/p/original/${
+            allTrending.results[Math.floor(Math.random() * allTrending.results.length)].poster_path
+          })`,
+        }}
+      >
+        <p className='relative z-20 uppercase text-3xl font-bold group-hover:underline'>Trending & Toplists</p>
+      </Link>
+      <div className='flex-1 flex gap-5 w-full'>
+        <Link
+          href='/movies'
+          className='relative w-full flex-1 flex items-center justify-center bg-cover bg-center bg-no-repeat rounded-md shadow-lg cursor-pointer group after:absolute after:w-full after:h-full after:bg-gradient-to-r after:from-blue-500/10 after:to-blue-900/70 after:rounded-md'
+          style={{
+            backgroundImage: `url(https://image.tmdb.org/t/p/original/${
+              movieTrending.results[Math.floor(Math.random() * movieTrending.results.length)].poster_path
+            })`,
+          }}
+        >
+          <p className='relative z-20 uppercase text-3xl font-bold group-hover:underline'>Movies</p>
+        </Link>
+        <Link
+          href='/tv'
+          className='relative w-full flex-1 flex items-center justify-center bg-cover bg-center bg-no-repeat rounded-md shadow-lg cursor-pointer group after:absolute after:w-full after:h-full after:bg-gradient-to-r after:from-blue-500/10 after:to-blue-900/70 after:rounded-md'
+          style={{
+            backgroundImage: `url(https://image.tmdb.org/t/p/original/${
+              tvTrending.results[Math.floor(Math.random() * tvTrending.results.length)].poster_path
+            })`,
+          }}
+        >
+          <p className='relative z-30 uppercase text-3xl font-bold group-hover:underline'>TV</p>
+        </Link>
       </div>
-    </>
+    </div>
   );
 };
 
