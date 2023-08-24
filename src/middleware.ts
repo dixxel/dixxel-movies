42aba7d8c7f9f5c007a4568e92d589f859ccc1d3
dough-land <viktor.melin@me.com>
2023-08-24 10:04:02 +0200

    refactor: update middleware for more paths

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-  publicRoutes: ['/', '/search', '/movie(.*)', '/tv(.*)', '/person(.*)'],
+  publicRoutes: ['/', '/search(.*)', '/all(.*)', '/movie(.*)', '/tv(.*)', '/person(.*)'],
 });
 
 export const config = {
